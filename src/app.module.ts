501dfe5239457f09ca808edef4feb99f90ba89dd
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2026-03-02 15:30:06 -0300

    feat: add Post and Comment models with corresponding migrations and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [UsersModule, AuthModule, PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
