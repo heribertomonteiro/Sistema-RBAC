fcdbc4ffe9ddd9a8707af51fe66dad219103359f
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2026-03-02 15:40:00 -0300

    feat: implement PostService with create method for post creation

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+
+    constructor(private prisma: PrismaService) {}
+
+    async create(post: { title: string, content: string, authorId: number }) {
+        return await this.prisma.post.create({
+            data: {
+                title: post.title,
+                content: post.content,
+                authorId: post.authorId,
+            },
+        });
+    }
+
+}
